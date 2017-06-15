f511f7636e96185d65ff00dc59781377633bfd50
cbrwflo <cbrwflo@users.noreply.github.com>
2017-06-14 21:31:30 -0700

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,44 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Pick up rocks',
+  //   completed: true
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // Find returns a cursor object
+  // db.collection('Users').find().toArray().then((docs) => {
+  //   console.log('Users');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //     console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Users').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //     console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: "Carl"}).toArray().then((docs) => {
+    console.log('Users named Carl:');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+      console.log('Unable to fetch todos', err);
+  });
+
+
+// will interfere with above count, apparently
+//  db.close();
+});
