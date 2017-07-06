26701cfc0346d0e6871acbb3ef71af5e6e5cae88
cbrwflo <cbrwflo@users.noreply.github.com>
2017-07-05 21:17:21 -0700

    Adds GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,11 +24,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
-//
-// app.get('/todos/', (req, res) => {
-//
-// });
-//
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 // app.get('/todos/:id', (req, res) => {
 //
 // });
