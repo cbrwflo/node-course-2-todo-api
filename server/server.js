f2485b556823a8ea4cd6f9b148d4f8fd33d7be91
cbrwflo <cbrwflo@users.noreply.github.com>
2017-07-08 14:46:14 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -32,9 +33,21 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// app.get('/todos/:id', (req, res) => {
-//
-// });
+app.get('/todos/:id', (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(req.params.id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send('{Error condition}');
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
