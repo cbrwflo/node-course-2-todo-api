eef3f025ffd76a6ead1b2f474b3728fcff0aff9c
Brown <Carl.Brown194@T-Mobile.com>
2019-08-15 06:08:42 -0700

    Fix Mongoose deprecated pushAll method warnings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+mongoose.plugin(schema => { schema.options.usePushEach = true });
 mongoose.connect(process.env.MONGODB_URI);
 
 module.export = {mongoose};
